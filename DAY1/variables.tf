e906062cdf15ece16b3656acbe152509392eb0e1
inyeza <inyeza1986@gmail.com>
2025-12-19 13:40:12 -0600

    Added dev.tfvars file

--- DAY1/variables.tf
+++ DAY1/variables.tf
@@ -1,18 +1,18 @@
 variable "aws_region" {
   description = "The AWS region to deploy resources in"
   type        = string
-  default     = "us-east-1"
+  #default     = "us-east-1"
 }
 
 variable "project-name" {
   description = "The name of the project"
   type        = string
-  default     = "test-project"
+  #default     = "test-project"
 
 }
 
 variable "environment" {
   description = "The environment type (e.g., dev, staging, prod)"
   type        = string
-  default     = "dev"
+  #default     = "dev"
 }
